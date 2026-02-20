5408452bd7c595b318831cf1ebcebc12ef43b8d9
sergey dyakonov <beefeater83@gmail.com>
2026-02-20 23:19:52 +0200

    Initial commit: clean project

---
+++ backend/src/Entity/Product.php
@@ -0,0 +1,83 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+#[ORM\Table(name: 'products')]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, nullable: false)]
+    #[Assert\NotBlank(groups: ['create', 'update', 'patch'])]
+    #[Groups(['create', 'update', 'patch'])]
+    private string $name;
+
+    #[ORM\Column(name: "price", type: Types::FLOAT, nullable: false)]
+    #[Assert\NotBlank(groups: ['create', 'update', 'patch'])]
+    #[Groups(['create', 'update', 'patch'])]
+    private float $price;
+
+    #[ORM\Column(length: 255, nullable: false)]
+    #[Groups(['create', 'update', 'patch'])]
+    private string $imagePath;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(?int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPrice(): float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getImagePath(): string
+    {
+        return $this->imagePath;
+    }
+
+    public function setImagePath(string $imagePath): static
+    {
+        $this->imagePath = $imagePath;
+
+        return $this;
+    }
+}
