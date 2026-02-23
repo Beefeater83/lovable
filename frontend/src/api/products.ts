2f80b377ba7f074dabcc61a689e4452fc3c00274
sergey dyakonov <beefeater83@gmail.com>
2026-02-23 13:03:27 +0200

    front example

---
+++ frontend/src/api/products.ts
@@ -0,0 +1,22 @@
+// src/api/products.ts
+export interface Product {
+    id: number
+    category: string
+    name: string
+    price: number
+    imagePath: string
+}
+
+export interface ProductsResponse {
+    items: Product[]
+    page: number
+    pageSize: number
+    total: number
+}
+
+// простой fetch к backend через Vite proxy
+export const getProducts = async (): Promise<ProductsResponse> => {
+    const res = await fetch('/api/products')
+    if (!res.ok) throw new Error('Failed to fetch products')
+    return res.json()
+}
