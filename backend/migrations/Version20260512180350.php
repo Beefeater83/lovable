f2c04b66791295854ce288e22315005701dcf2fc
sergey dyakonov <beefeater83@gmail.com>
2026-05-13 10:43:51 +0300

    product uuid v7

--- backend/migrations/Version20260512180350.php
+++ backend/migrations/Version20260512180350.php
@@ -12,12 +12,10 @@ final class Version20260512180350 extends AbstractMigration
     public function up(Schema $schema): void
     {
         $this->addSql('ALTER TABLE products CHANGE id id BINARY(16) NOT NULL');
-        $this->addSql('ALTER TABLE products ADD CONSTRAINT FK_B3BA5A5AA76ED395 FOREIGN KEY (user_id) REFERENCES users (id)');
     }
 
     public function down(Schema $schema): void
     {
-        $this->addSql('ALTER TABLE products DROP FOREIGN KEY FK_B3BA5A5AA76ED395');
         $this->addSql('ALTER TABLE products CHANGE id id INT AUTO_INCREMENT NOT NULL');
     }
 }
